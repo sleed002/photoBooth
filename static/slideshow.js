d5f9da2ccc5f31bca0eccfe7c7723b027dee82ad
Debbie Sleeper <Deborah.I.Sleeper@disney.com>
2018-05-07 15:49:25 -0700

    slideshow added

--- static/slideshow.js
+++ static/slideshow.js
@@ -1,24 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 var Button = document.getElementById('button');
+var Stop = document.getElementById('stop');
 
 var slideIndex = 0;
 
+function stopSlides() {
+  location.reload()
+}
+
 function showSlides() {
     document.getElementById("party").play();
+
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+        slides[i].style.height = "500px";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 1800); // Change image every 2 seconds
+
   } //https://www.w3schools.com/howto/howto_js_slideshow.asp
 
   Button.addEventListener('click', function () {
-    showSlides();
-    button.innerText = "Stop Slideshow"
+      showSlides();
+      Button.style.display = "none"
+      Stop.style.visibility = "visible"
+  });
+
+  Stop.addEventListener('click', function () {
+      stopSlides();
   });
 })
